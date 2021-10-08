9fb0b14f2475a6812f9d0048d6c74d45fffacb03
Maksim <eliott_nu@mail.ru>
2021-10-09 02:41:32 +0900

    Connect to server (almost)

---
+++ ChatApp/ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.Net;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server _server;
+        
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+        }
+    }
+}
