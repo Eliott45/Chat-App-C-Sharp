203a1ff7e059e0c2ebadd6794309379c4bb0ce37
Maksim <eliott_nu@mail.ru>
2024-10-09 23:59:08 +0900

    Clean up MainViewModel for client

--- ChatApp/ChatApp/MVVM/ViewModel/MainViewModel.cs
+++ ChatApp/ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ChatClient.MVVM.Core;
+﻿using System;
+using ChatClient.MVVM.Core;
 using ChatClient.MVVM.Model;
 using ChatClient.Net;
 using System.Collections.ObjectModel;
@@ -7,7 +8,7 @@ using System.Windows;
 
 namespace ChatClient.MVVM.ViewModel;
 
-internal class MainViewModel : ObservableObject
+internal class MainViewModel : ObservableObject, IDisposable
 {
     public ObservableCollection<UserModel> Users { get; set; }
     public ObservableCollection<string> Messages { get; set; }
@@ -37,21 +38,49 @@ internal class MainViewModel : ObservableObject
         Users = new ObservableCollection<UserModel>();
         Messages = new ObservableCollection<string>();
         _server = new Server();
-        _server.ConnectedEvent += UserConnected;
-        _server.MessageReceivedEvent += MessageReceived;
-        _server.UserDisconnectEvent += UserDisconnect;
 
-        ConnectToServerCommand = new RelayCommand(o =>
-        {
-            _server.ConnectToServer(IpAddress, Username);
-        }, o => !string.IsNullOrEmpty(IpAddress) && !string.IsNullOrEmpty(Username));
-
-        SendMessageCommand = new RelayCommand(o => {
-            _server.SendMessageToServer(Message);
-        }, o => !string.IsNullOrEmpty(Message));
+        SubscribeToServer();
+        CreateServerCommands();
     }
 
-    private void UserConnected()
+    public void Dispose()
+    {
+        UnsubscribeFromServer();
+    }
+
+    private void SubscribeToServer()
+    {
+        _server.ConnectedEvent += OnUserConnected;
+        _server.MessageReceivedEvent += OnMessageReceived;
+        _server.UserDisconnectEvent += OnUserDisconnect;
+    }
+
+    private void UnsubscribeFromServer()
+    {
+        _server.ConnectedEvent -= OnUserConnected;
+        _server.MessageReceivedEvent -= OnMessageReceived;
+        _server.UserDisconnectEvent -= OnUserDisconnect;
+    }
+
+    private void CreateServerCommands()
+    {
+        ConnectToServerCommand = new RelayCommand(_ => ConnectToServer(), _ => CanConnectToServer());
+        SendMessageCommand = new RelayCommand(_ => SendMessageToServer(), _ => CanSendMessage());
+    }
+
+    private void ConnectToServer() =>
+        _server.ConnectToServer(IpAddress, Username);
+
+    private void SendMessageToServer() =>
+        _server.SendMessageToServer(Message);
+
+    private bool CanConnectToServer() =>
+        !string.IsNullOrEmpty(IpAddress) && !string.IsNullOrEmpty(Username);
+
+    private bool CanSendMessage() =>
+        !string.IsNullOrEmpty(Message);
+
+    private void OnUserConnected()
     {
         var user = new UserModel
         {
@@ -65,16 +94,17 @@ internal class MainViewModel : ObservableObject
         }
     }
 
-    private void UserDisconnect()
+    private void OnUserDisconnect()
     {
         var uid = _server.PacketReader.ReadMessage();
         var user = Users.FirstOrDefault(x => x.Uid == uid);
         Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
     }
 
-    private void MessageReceived()
+    private void OnMessageReceived()
     {
         var msg = _server.PacketReader.ReadMessage();
+
         Application.Current.Dispatcher.Invoke(() =>
         {
             Messages.Add(msg);
